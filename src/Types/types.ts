7e9c65d2915e7599b35d0eaea7e45d052507877f
RuralGiovane <giovaneamato@gmail.com>
2026-06-03 17:36:27 -0300

    Feat: Typing navigation props

---
+++ src/Types/types.ts
@@ -0,0 +1,46 @@
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+
+export type RootStackParamList = {
+  LoginScreen: undefined;
+  RegisterScreen: undefined;
+  DashboardScreen: undefined;
+  MyBatchScreen: undefined;
+  NewBatchScreen: undefined;
+  ForecastScreen: undefined;
+  BatchDetails: undefined;
+};
+
+export type LoginScreenProps = NativeStackScreenProps<
+  RootStackParamList,
+  'LoginScreen'
+>;
+
+export type RegisterScreenProps = NativeStackScreenProps<
+  RootStackParamList,
+  'RegisterScreen'
+>;
+
+export type DashboardScreenProps = NativeStackScreenProps<
+  RootStackParamList,
+  'DashboardScreen'
+>;
+
+export type MyBatchScreenProps = NativeStackScreenProps<
+  RootStackParamList,
+  'MyBatchScreen'
+>;
+
+export type BatchDetailsProps  = NativeStackScreenProps<
+  RootStackParamList,
+  'BatchDetails'
+>;  
+
+export type NewBatchScreenProps  = NativeStackScreenProps<
+  RootStackParamList,
+  'NewBatchScreen'
+>;
+
+export type ForecastScreenProps  = NativeStackScreenProps<
+  RootStackParamList,
+  'ForecastScreen'
+>;
