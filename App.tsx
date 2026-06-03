422afc5f0e7abbb8f5f10c100ad50823a92f3b5b
RuralGiovane <giovaneamato@gmail.com>
2026-06-03 19:22:03 -0300

    Feat: Creating AppNavigatior

--- App.tsx
+++ App.tsx
@@ -1,10 +1,16 @@
 import './global.css'; 
-import { Text, View } from 'react-native';
+import AppNavigator from "./src/Navigation/AppNavigator"
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white p-4">
-      <Text className="text-lg font-bold">Num é que rodou mesmo</Text>
-    </View>
+    <SafeAreaProvider>
+      <AuthProvider>
+        <NavigationContainer>
+            <AppNavigatior />
+        </NavigationContainer>
+      </AuthProvider>
+    </SafeAreaProvider>
   );
 }
---
+++ src/Navigation/AppNavigator.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { LoaderCircle } from "lucide-react-native"
+
+// Navegação pré/pós login
+import { useAuth } from "../Context/ProtectedRoute";
+
+// Import das Screens
+import LoginScreen from "../Screens/LoginScreen";
+import RegisterScreen from "../Screens/RegisterScreen";
+import DashboardScreen from "../Screens/DashboardScreen";
+import MyBatchScreen from "../Screens/Batchs/MyBatchScreens";
+import NewBatchScreen from "../Screens/Batchs/NewBatchScreen";
+import BatchDetails from "../Screens/Batchs/BatchDetails";
+import ForecastScreen from "../Screens/ForecastScreen";
+import Header from "../Components/Header";
+import { RootStackParamList } from "../Types/types";
+import BottomTabs from '../Components/BottomTabs';
+ 
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export function AppNavigator() {
+    const { loggedIn, loading } = useAuth();
+
+    if (loading) return <LoaderCircle />; // Aguarde carregar
+
+    return (
+        <Stack.Navigator>
+            {loggedIn ? (
+                // Stack para usuário autenticado
+                <Stack.Group screenOptions={{ header: () => <Header /> }}>
+                    <Stack.Screen name="DashboardScreen" component={DashboardScreen} />
+                    <Stack.Screen name="MyBatchScreen" component={MyBatchScreen} />
+                    <Stack.Screen name="NewBatchScreen" component={NewBatchScreen} />
+                    <Stack.Screen name="ForecastScreen" component={ForecastScreen} />
+                    <Stack.Screen name="BatchDetails" component={BatchDetails} />
+                </Stack.Group>
+            ) : (
+                // Stack para usuário NÃO autenticado
+                <Stack.Group screenOptions={{ headerShown: false }}>
+                    <Stack.Screen name="LoginScreen" component={LoginScreen} />
+                    <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+                </Stack.Group>
+            )}
+        </Stack.Navigator>
+    );
+}
