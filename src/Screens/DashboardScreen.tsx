313adc93ace7bcbcf1ccd223859c46cfe962c7a4
RuralGiovane <giovaneamato@gmail.com>
2026-06-04 14:46:25 -0300

    Chore: Adding Logout Button just for test

--- src/Screens/DashboardScreen.tsx
+++ src/Screens/DashboardScreen.tsx
@@ -1,9 +1,18 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from "react-native";
+import { DashboardScreenProps } from "../Types/types";
+import { useAuth } from "../Context/ProtectedRoute";
 
-export default function DashboardScreen() {
-    return (
-        <View className="flex-1 items-center justify-center">
-            <Text>Dashboard Screen</Text>
-        </View>
-    )
+export function DashboardScreen({ navigation }: DashboardScreenProps) {
+  const { logout } = useAuth();
+  const handleLogout = () => {
+    logout();
+  };
+
+  return (
+    <View className="flex-1 items-center justify-center">
+      <TouchableOpacity onPress={handleLogout}>
+        <Text>Go to Login</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
