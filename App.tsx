8d10e6b59306ab2fbeea1bd16a93c8ae7f86d551
RuralGiovane <giovaneamato@gmail.com>
2026-06-04 14:51:35 -0300

    Chore: Changing Import Route

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import './global.css';
 import { AppNavigator } from "./src/Navigation/AppNavigator"
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
-import { AuthProvider } from './src/Context/ProtectedRoute';
+import { AuthProvider } from './src/Context/AuthContext';
 
 export default function App() {
   return (
