303496484b27324d5bdac564d2e346e46b3f80de
RuralGiovane <giovaneamato@gmail.com>
2026-06-09 20:40:10 -0300

    Chore: Configurating IP

--- src/config.ts
+++ src/config.ts
@@ -4,15 +4,14 @@
 
 import { Platform } from "react-native";
 
+const SEU_IP_LOCAL = "SEU-IP-LOCAL"; // <- SE ESTIVER NO CELULAR FÍSICO, MUDE ESTE IP
+
 export const JAVA_API_BASE_URL =
   Platform.OS === "android"
-    ? "http://10.0.2.2:3000" // Android Emulator
+    ? "http://localhost:8080/" // Android Emulator ou Físico (Java API)
     : "https://guia-safra-api.onrender.com/"; // iOS Simulator e Web (Java API)
 
 export const DOTNET_API_BASE_URL =
   Platform.OS === "android"
-    ? "http://10.0.2.2:5128" // Android Emulator (.NET API)
-    : "http://localhost:5128"; // iOS Simulator e Web (.NET API)
-
-// 👇 Se você for usar celular físico, COMENTE as linhas acima e DESCOMENTE:
-// export const API_BASE_URL = "http://192.168.0.10:3000";
+    ? `http://${SEU_IP_LOCAL}:5128` // Android Emulator ou Físico (.NET API)
+    : `http://0.0.0.0:5128/`; // iOS Simulator e Web (.NET API)
