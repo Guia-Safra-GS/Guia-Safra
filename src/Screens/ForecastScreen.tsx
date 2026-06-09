e304a07370cfd2a2fea0c94a628dc9ae781df6a7
RuralGiovane <giovaneamato@gmail.com>
2026-06-09 17:20:21 -0300

    Feat: Adding Forecast Components

--- src/Screens/ForecastScreen.tsx
+++ src/Screens/ForecastScreen.tsx
@@ -1,9 +1,16 @@
-import { View, Text } from 'react-native'
+import { View, ScrollView } from 'react-native'
+import { ForecastCarrouselCard } from '../Components/ForecastComponents/ForecastCarrouselCard'
+import { RainProbability } from '../Components/ForecastComponents/RainProbability'
+import { IrrigationHistory } from "../Components/ForecastComponents/IrrigationHistory"
 
 export function ForecastScreen() {
     return (
-        <View className="flex-1 items-center justify-center">
-            <Text>Forecast Screen</Text>
+        <View className="flex-1 items-center justify-center px-4">
+            <ScrollView showsVerticalScrollIndicator={false} className="w-full">
+                <ForecastCarrouselCard />
+                <RainProbability />
+                <IrrigationHistory />
+            </ScrollView>
         </View>
     )
 }
