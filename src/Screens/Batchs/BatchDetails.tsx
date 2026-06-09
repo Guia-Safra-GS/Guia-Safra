65b5c5819dc29a7e1163a46c0ccbe676a94b04ef
RuralGiovane <giovaneamato@gmail.com>
2026-06-09 03:10:40 -0300

    Adding components to Batch details Screen

--- src/Screens/Batchs/BatchDetails.tsx
+++ src/Screens/Batchs/BatchDetails.tsx
@@ -1,9 +1,22 @@
 import { View, Text } from 'react-native'
+import { DeleteBatch } from '../../Components/BatchDetailComponents/DeleteBatch';
+import { WateringLimitsButton } from '../../Components/BatchDetailComponents/WateringLimitsButton';
+import { BatchHumidity } from '../../Components/BatchDetailComponents/BatchHumidity';
+import { SoilTemperature } from '../../Components/BatchDetailComponents/SoilTemperature';
+import { BatchIdentifier } from '../../Components/BatchDetailComponents/BatchIdentifier';
 
-export function BatchDetails() {
+
+export function BatchDetails({ route }: { route: any }) {
+    const { sensorId, type, status } = route.params;
     return (
-        <View className="flex-1 items-center justify-center">
-            <Text>Batch Details Screen</Text>
+        <View className="flex-1 w-full items-center justify-center px-2">
+            <BatchIdentifier sensorId={sensorId} type={type} status={status} />
+            <View className="flex-row w-full justify-center py-4 rounded-3xl shadow-sm mb-4 gap-4">
+                <BatchHumidity />
+                <SoilTemperature />
+            </View>
+            <WateringLimitsButton />
+            <DeleteBatch />
         </View>
     )
 }
--- src/Screens/Batchs/MyBatchScreens.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native'
-
-export function MyBatchScreen() {
-    return (
-        <View className="flex-1 items-center justify-center">
-            <Text>My Batch Screen</Text>
-        </View>
-    )
-}
