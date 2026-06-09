53f338f269b04ccb05a74f3b55e5900613261812
RuralGiovane <giovaneamato@gmail.com>
2026-06-09 19:10:13 -0300

    Feat: Creating File

---
+++ src/Components/BatchDetailComponents/EditFormWateringLimits.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+
+
+export function EditFormWateringLimits() {
+    return (
+        <View>  
+            <Text>Editar Limites de Rega</Text>
+        </View>
+    )
+}
