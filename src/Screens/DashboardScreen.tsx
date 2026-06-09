fd77185c2acaf739c01a9d973a826aa5dcbf3257
RuralGiovane <giovaneamato@gmail.com>
2026-06-09 03:12:00 -0300

    Style: Hiding vertical ScrollBar

--- src/Screens/DashboardScreen.tsx
+++ src/Screens/DashboardScreen.tsx
@@ -1,8 +1,4 @@
 import { Text, View, ScrollView } from "react-native";
-import { useAuth } from "../Context/AuthContext";
-
-import { SafeAreaView, useSafeAreaInsets } from "react-native-safe-area-context";
-
 //Importando Cards
 import { ForecastCard } from "../Components/DasboardComponents/ForecastCard";
 import { SoilMoisture } from "../Components/DasboardComponents/SoilMoisture";
@@ -10,11 +6,11 @@ import { RecentAlerts } from "../Components/DasboardComponents/RecentAlerts";
 
 export function DashboardScreen() {
   return (
-  <ScrollView>
-    <View className="flex-1 items-center justify-center px-4 gap-5 py-4">
-      <View className="items-start justify-start w-full">
+  <ScrollView showsVerticalScrollIndicator={false} >
+    <View className="items-start justify-start w-full px-4 py-4">
         <Text className="text-lg text-gray-500">Resumo da sua propriedade hoje</Text>
-      </View>
+    </View>
+    <View className="flex-1 items-center justify-center px-4 gap-5 py-4">
       <ForecastCard />
       <SoilMoisture />
       <RecentAlerts  />
