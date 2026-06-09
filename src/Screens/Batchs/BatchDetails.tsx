1f67a0d634ce5726df3dea8a5f2fa6b4e3fa2c8f
RuralGiovane <giovaneamato@gmail.com>
2026-06-09 18:54:33 -0300

    Feat: Addign Sensor activity component

--- src/Screens/Batchs/BatchDetails.tsx
+++ src/Screens/Batchs/BatchDetails.tsx
@@ -4,17 +4,18 @@ import { WateringLimitsButton } from '../../Components/BatchDetailComponents/Wat
 import { BatchHumidity } from '../../Components/BatchDetailComponents/BatchHumidity';
 import { SoilTemperature } from '../../Components/BatchDetailComponents/SoilTemperature';
 import { BatchIdentifier } from '../../Components/BatchDetailComponents/BatchIdentifier';
-
+import { SensorActivity } from '../../Components/BatchDetailComponents/SensorActivity';
 
 export function BatchDetails({ route }: { route: any }) {
     const { sensorId, type, status } = route.params;
     return (
         <View className="flex-1 w-full items-center justify-center px-2">
             <BatchIdentifier sensorId={sensorId} type={type} status={status} />
-            <View className="flex-row w-full justify-center py-4 rounded-3xl shadow-sm mb-4 gap-4">
+            <View className="flex-row w-screen justify-center py-4 shadow-sm gap-4">
                 <BatchHumidity />
                 <SoilTemperature />
             </View>
+            <SensorActivity />
             <WateringLimitsButton />
             <DeleteBatch />
         </View>
